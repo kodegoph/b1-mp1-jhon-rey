77312c33a7ef8ec29ff8ce3e6f825b2a78522370
Jhon Rey <jhonreymendiola@gmail.com>
2021-06-11 17:16:30 +0800

    made carousel, and  fix responsiveness on ipad

---
+++ script.js
@@ -0,0 +1,61 @@
+  /* When the user clicks on the button, 
+      toggle between hiding and showing the dropdown content */
+      function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+
+
+//  
+// carousel
+// 
+      var slideIndex = 1;
+showSlides(slideIndex);
+
+   var modal = document.getElementById("myModal");
+
+    function openPopup(img) {
+      var modalImg = document.getElementById("img01");
+      var captionText = document.getElementById("caption");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+    }
+
+    function closePopup() {
+      modal.style.display = "none";
+    }
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+}
